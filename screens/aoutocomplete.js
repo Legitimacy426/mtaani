e197e2ac4d2026e7d10a04c17c358e84f0dd1305
legitimacy426 <legitimacy426@gmail.com>
2023-04-06 21:00:15 +0300

    changed api key

--- screens/aoutocomplete.js
+++ screens/aoutocomplete.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import React from "react";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
 const GooglePlacesInput = () => {
   return (
     <GooglePlacesAutocomplete
-      placeholder='Search'
+      placeholder="Search"
       onPress={(data, details = null) => {
         // 'details' is provided when fetchDetails = true
         console.log(data, details);
       }}
       query={{
-        key: 'YOUR API KEY',
-        language: 'en',
+        key: "",
+        language: "en",
       }}
     />
   );
